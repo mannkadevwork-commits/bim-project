8a24917873e1366ed7025b63a9ef3ccf4359d66e
mannkadevwork-commits <mannkadevwork@gmail.com>
2026-09-15 13:18:00 +0530

    optimized frontend for glb

--- xeo-kit-project/bim-viewer-app/src/main_25_09.jsx
+++ xeo-kit-project/bim-viewer-app/src/main_25_09.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './engine/installMeshoptDecoder.js'
 import App from './App.jsx'
 import './index.css'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
